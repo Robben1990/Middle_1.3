8cb5f96757c71c1236844a46f9eae76817a8da32
Robben1990 <smolskiy90@gmail.com>
2021-07-19 14:34:34 +0300

    may 1

---
+++ Middle_1.2.cpp
@@ -0,0 +1,81 @@
+// Middle_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+
+class Vector
+{
+public:
+	Vector()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	Vector(float x, float y, float z)
+	{
+		this -> x = x;
+		this -> y = y;
+		this -> z = z;
+	}
+	friend bool operator>(const Vector& A, const Vector& B);
+	
+	friend Vector operator+(const Vector& a, const Vector& b);
+
+	friend std::ostream& operator<< (std::ostream& out,const Vector& A);
+
+	operator float()
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+	
+	float operator[]( int index)
+	{
+		switch (index)
+		{
+		case 0: return x; break;
+		case 1: return y; break;
+		case 2: return z; break;
+		default: std::cout << "Index error";
+			break;
+		}
+		
+	}
+private:
+	float x;
+	float y;
+	float z;
+};
+bool operator>(const Vector& A, const Vector& B)
+{
+	return false;
+}
+
+Vector operator+(const Vector& a,const Vector& b)
+{
+	return Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+std::ostream& operator<< (std::ostream& out,const Vector& A)
+{
+	out << ' ' << A.x << ' ' << A.y << ' ' << A.z;
+	return out;
+}
+
+
+int main()
+{
+	Vector A(1.0f, 2.0f, 5.0f);
+	Vector B(4.0f, 5.0f, 10.0f);
+	Vector C;
+	C = B + A;
+	std::cout << A + B<<'\n';
+	std::cout << B[1];
+	std::cout << float(C)<<'\n';
+
+	std::cout << "A length is " << static_cast <float>(A);
+
+}
+
+
